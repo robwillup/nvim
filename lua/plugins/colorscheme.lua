78888105834e428bb937636b196658e37c0f8df0
Robson William <robson.dasilva@plurilock.com>
2023-06-06 13:50:34 -0700

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+    -- add gruvbox
+    { "ellisonleao/gruvbox.nvim" },
+    { "pineapplegiant/spaceduck" },
+    { "hiroakis/cyberspace.vim" },
+
+    -- Configure LazyVim to load gruvbox
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "cyberspace",
+      },
+    }
+  }
