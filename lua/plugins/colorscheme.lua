d82f3cd3f6d111cfbd26002d273e9af130b8c73e
Robson William <robson.dasilva@plurilock.com>
2023-06-07 15:56:49 -0700

    Adding more color schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,16 @@ return {
     { "ellisonleao/gruvbox.nvim" },
     { "pineapplegiant/spaceduck" },
     { "hiroakis/cyberspace.vim" },
+    { "nanotech/jellybeans.vim"},
+    { "caiocutrim/black-coffee-molokai"},
+    { "mawkler/onedark.nvim"},
+    { "ayu-theme/ayu-vim"},
 
     -- Configure LazyVim to load gruvbox
     {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "spaceduck",
+        colorscheme = "cyberspace",
       },
     }
   }
